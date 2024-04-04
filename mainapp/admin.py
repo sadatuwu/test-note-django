955b49b97c1c5debfd59704beca21cd8a6571291
sadat <shameemsadat00@gmail.com>
2024-04-04 17:52:16 +0600

    first commit

---
+++ mainapp/admin.py
@@ -0,0 +1,5 @@
+# mainapp/admin.py
+from django.contrib import admin
+from .models import Task
+
+admin.site.register(Task)
---
+++ mainapp/urls.py
@@ -0,0 +1,8 @@
+# mainapp/urls.py
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('tasks/', views.task_list, name='task-list'),
+    path('tasks/<int:pk>/', views.task_detail, name='task-detail'),
+]
